d75099b4074bbcdd416f15ce94526ecba0b6ffaf
Eugene-prog <ieugene.prog@gmail.com>
2024-08-17 23:50:13 +0700

    Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkArrayEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("empty");
+    }
+
+    @Test
+    void checkTheNameContainsEqual() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key1value1", "key2=value2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("=");
+    }
+
+    @Test
+    void checkTheNameNotContainsKey() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("=key1=value1", "key2=value2"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("key");
+    }
+
+    @Test
+    void checkTheNameNotContainsValue() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(() -> nameLoad.parse("key1=value1", "key2="))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("value");
+    }
+}
