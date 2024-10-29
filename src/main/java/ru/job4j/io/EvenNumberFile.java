aa8882c1c5770bf5d1dac0dbe8673d3cace893fc
Eugene-prog <ieugene.prog@gmail.com>
2024-10-29 22:02:23 +0700

    fix код без использования сканера. FileInputStream [#4898]. В классе нужно прочитать файл even.txt. Для каждого числа проверить является ли оно четным числом.

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -2,21 +2,39 @@ package ru.job4j.io;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class EvenNumberFile {
+    private static void checkAndPrintEvenOrOdd(String numberStr) {
+        try {
+            int number = Integer.parseInt(numberStr);
+            if (number % 2 == 0) {
+                System.out.println(number + " - число четное");
+            } else {
+                System.out.println(number + " - число нечетное");
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         try (FileInputStream input = new FileInputStream("data/even.txt")) {
-            Scanner scanner = new Scanner(input);
-            while (scanner.hasNextInt()) {
-                int number = scanner.nextInt();
-                if (number % 2 == 0) {
-                    System.out.println(number + " - число чётное");
+            StringBuilder numberBuffer = new StringBuilder();
+            int read;
+            while ((read = input.read()) != -1) {
+                char ch = (char) read;
+                if (Character.isDigit(ch)) {
+                    numberBuffer.append(ch);
                 } else {
-                    System.out.println(number + " - число нечётное");
+                    if (!numberBuffer.isEmpty()) {
+                        checkAndPrintEvenOrOdd(numberBuffer.toString());
+                        numberBuffer = new StringBuilder();
+                    }
                 }
             }
-            scanner.close();
+            if (!numberBuffer.isEmpty()) {
+                checkAndPrintEvenOrOdd(numberBuffer.toString());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
