d6b38b3e0313e7f6afd5cf30a32e4bcef9d8de0b
Eugene-prog <ieugene.prog@gmail.com>
2024-11-03 17:11:39 +0700

    BufferedOutputStream [#252490] Нужно добавить метод saveTo(String out). Метод должен записывать результат работы метода filter() в файл out.

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,8 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,8 +27,19 @@ public class LogFilter {
         return result;
     }
 
+    public void saveTo(String out) {
+        var data = filter();
+        try (PrintWriter output =
+                     new PrintWriter(new BufferedOutputStream(new FileOutputStream(out)))) {
+            for (String line : data) {
+                output.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter("data/log.txt");
-        logFilter.filter().forEach(System.out::println);
+        new LogFilter("data/log.txt").saveTo("data/404.txt");
     }
 }
