0620100695d51b132acd373710e228a28b6f4eba
Eugene-prog <ieugene.prog@gmail.com>
2024-11-06 20:57:37 +0700

    fix Анализ доступности сервера [#859] упростить метод unavailable()

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,36 +6,21 @@ import java.time.LocalTime;
 public class Analysis {
 
     public void unavailable(String source, String target) {
-        try (PrintWriter output =
-                     new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+        try (BufferedReader input = new BufferedReader(new FileReader(source));
+             PrintWriter output = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            String line;
             boolean isWork = true;
-            StringBuilder result = new StringBuilder();
-            try (BufferedReader input = new BufferedReader(new FileReader(source))) {
-                String line;
-                while ((line = input.readLine()) != null) {
-                    line = line.trim();
-                    int index = line.indexOf(' ');
-                    if (index == -1) {
-                        continue;
-                    }
-                    int status = Integer.parseInt(line.substring(0, index).trim());
-                    LocalTime t = LocalTime.parse(line.substring(index + 1).trim());
-                    if ((status >= 400) && (isWork)) {
-                        isWork = false;
-                        result.append(t).append(";");
-                    } else if ((status < 400) && (!isWork)) {
-                        isWork = true;
-                        result.append(t).append(";");
-                        output.println(result);
-                        result = new StringBuilder();
-                    }
+            while ((line = input.readLine()) != null) {
+                int index = line.indexOf(' ');
+                int status = Integer.parseInt(line.substring(0, index).trim());
+                LocalTime time = LocalTime.parse(line.substring(index + 1).trim());
+                if (status >= 400 && isWork) {
+                    output.print(time + ";");
+                    isWork = false;
+                } else if (status < 400 && !isWork) {
+                    output.println(time + ";");
+                    isWork = true;
                 }
-                if (!isWork && !result.isEmpty()) {
-                    result.append("unknown").append(";");
-                    output.println(result);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
