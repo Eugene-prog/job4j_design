7e033cffe1c8f7cbae8bee6c97471e4ca52fa047
Eugene-prog <ieugene.prog@gmail.com>
2024-11-19 23:07:21 +0700

    Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -9,8 +10,30 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("c:\\test\\");
-        search(start, path -> path.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Не заданы параметры запуска");
+        }
+        try {
+            Path start = Paths.get(args[0]);
+            if (!Files.exists(start)) {
+                throw new IllegalArgumentException("Указанный путь не существует: " + args[0]);
+            }
+            if (!Files.isDirectory(start)) {
+                throw new IllegalArgumentException("Указанный путь не является директорией: " + args[0]);
+            }
+        } catch (InvalidPathException e) {
+            throw new IllegalArgumentException("Указан некорректный путь: " + args[0], e);
+        }
+
+        Path start = Paths.get(args[0]);
+        String fileExtension = args[1];
+        if (fileExtension.isEmpty()) {
+            throw new IllegalArgumentException("Расширение файла не должно быть пустым.");
+        }
+        if (fileExtension.startsWith(".")) {
+            throw new IllegalArgumentException("Расширение файла не должно начинаться с точки.");
+        }
+        search(start, path -> path.toFile().getName().endsWith("." + fileExtension)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
